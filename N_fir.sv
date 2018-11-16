66bcd9a1aa4c0c46df469f119079ef1eb838d959
kyuri95 <38845321+kyuri95@users.noreply.github.com>
2018-11-15 22:39:55 -0800

    FINAL VERSION

--- N_fir.sv
+++ N_fir.sv
@@ -1,40 +1,53 @@
- module N_fir #(parameter N=16) (reset, CLOCK_50, data_in, data_out, read, write);
+// filter with size of N
+ module N_fir #(parameter N=16) (reset, CLOCK_50, data_in, data_out, read);
 	
-	input logic reset, CLOCK_50, read, write;
-	input logic [23:0] data_in;
-	output logic [23:0] data_out;
+	input logic reset, CLOCK_50, read;
+	input logic signed [23:0] data_in;
+	output logic signed [23:0] data_out;
 	
 	parameter DATA_SIZE = 24;
 	
-	logic rd, wr, empty, full;
-	logic [23:0] w_data, r_data;
-	logic [23:0] accumulator_in, accumulator_out;
+	logic empty, full, full_temp;
+	logic signed [23:0] w_data, r_data; // FIFO data
+	logic signed [23:0] accumulator_in, accumulator_out; // Accumulator data
 	
 	// Divide input by N
 	assign w_data = data_in / N;
+	assign full_temp = full;
 	
 	// Store in FIFO
-	fifo #(.DATA_WIDTH(DATA_SIZE), .ADDR_WIDTH(4)) fifo_unit
-   (.clk(CLOCK_50), .reset, .rd(write), .wr(read), .w_data, .empty, .full, .r_data);
+	// Only write when read is true & only read when read is true and buffer is full
+	fifo #(.DATA_WIDTH(DATA_SIZE), .ADDR_WIDTH($clog2(N))) fifo_unit
+   (.clk(CLOCK_50), .reset, .rd(full_temp && read), .wr(read), .w_data, .empty, .full, .r_data);
 	
+	// Accumulator logic
+	always_comb begin
+		if(full_temp)
+			accumulator_in = accumulator_out + (w_data - r_data);
+		else 
+			accumulator_in = accumulator_out + w_data;
+	end
 
 	always_ff @(posedge CLOCK_50) begin
-		if(reset) begin // fill buffer all zeros
-			accumulator_in <= 0;
-			accumulator_out <= 0;
+		if(reset) begin 
+			accumulator_out <= 0; // Initialize to zero
 		end else begin
-			accumulator_out <= accumulator_in;
-			accumulator_in <= accumulator_out + (w_data - r_data);
+			if(read) begin // Update data
+				accumulator_out <= accumulator_in;
+				data_out <= accumulator_in;
+			end
 		end
+		
 	end
 	
-	assign data_out = accumulator_in;
+
 	
 endmodule
 
 module tb_N_fir();
-	logic reset, CLOCK_50;
-	logic [23:0] data_in, data_out;
+	logic reset, CLOCK_50, read;
+	logic signed [23:0] data_in;
+	logic signed [23:0] data_out;
 	
 	parameter SIZE = 16;
 	
@@ -48,11 +61,12 @@ module tb_N_fir();
 	end
 
 	initial begin
-		reset <= 1; @(posedge CLOCK_50);
-		reset <= 0;
-		for (int i = 0; i < SIZE; i++) 
+		reset <= 1; read <= 0;@(posedge CLOCK_50);
+		reset <= 0; read <= 1;
+		for (int i = 0; i < SIZE * 2 + 1; i++) begin
 			data_in <= 24'h000400; @(posedge CLOCK_50);
-		for (int i = 0; i < SIZE/2; i++) begin
+		end
+		for (int j = 0; j < SIZE + 1; j++) begin
 			data_in <= 24'h000800; @(posedge CLOCK_50);
 			data_in <= 24'h000000; @(posedge CLOCK_50);
 		end
